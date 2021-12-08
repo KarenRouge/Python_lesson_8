f31c250a92c16e4721db32516a2aa10eab134541
KarenRouge <karenrouge1991@gmail.com>
2021-12-08 12:28:58 -0500

    Task_7 complete

--- Task_7.py
+++ Task_7.py
@@ -2,18 +2,23 @@ class ComplexNumber:
     def __init__(self, a, b):
         self.a = a
         self.b = b
+        self.a = self.a.split('+')
+        self.a[1] = self.a[1].replace('j', '')
+        self.b = self.b.split('+')
+        self.b[1] = self.b[1].replace('j', '')
 
-    def __add__(self, other):
-        return f'сумма двух комплексных чисел равна: ({self.a + other.a}, {self.b + other.b}).'
+    def __add__(self):
 
-    def __mul__(self, other):
-            return f'произведение двух комплексных чисел равна: ({self.a * other.a - (self.b * other.b)}, {self.b * other.a}).'
+        return f'сумма двух комплексных чисел равна: ({int(self.a[0]) + int(self.b[0])}+{int(self.a[1]) + int(self.b[1])}j).'
+
+    def __mul__(self):
+
+        return f'произведение двух комплексных чисел равно: ' \
+               f'({int(self.a[0]) * int(self.b[0]) - int(self.a[1]) * int(self.b[1])}' \
+        f'+{int(self.b[0]) * int(self.a[1]) + int(self.a[0]) * int(self.b[1])}j)'
 
 
+z_1 = ComplexNumber('1+2j', '1+3j')
+print(z_1.__add__())
+print(z_1.__mul__())
 
-
-z_1 = ComplexNumber(1, -2)
-z_2 = ComplexNumber(3, 4)
-
-print(z_1 + z_2)
-print(z_1 * z_2)
